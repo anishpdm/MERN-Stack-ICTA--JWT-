c196e6cc82aa69fff49ba688227f042baf6c52cd
anishpdm <anish.vilayil.s@gmail.com>
2022-02-08 20:00:03 +0530

    API Completed

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const express=require('express')
 const bodyParser=require('body-parser')
 const mongoose=require('mongoose')
 const {userModel}=require('./models/users')
-var jwt = require('jsonwebtoken');
+const bcrpt=require('bcrypt')
+const jwt=require('jsonwebtoken')
 
 
 let app = express()
@@ -25,6 +26,23 @@ mongoose.connect('mongodb+srv://userone:userone@cluster0.vcc0q.mongodb.net/BlogA
     useUnifiedTopology: true
   } );
 
+  app.post('/api/blogpost',(req,res)=>{
+
+    console.log(req.body)
+
+    jwt.verify(req.headers.token,'ictacademy',(err,decoded)=>{
+        if(decoded && decoded.email ){
+            console.log(req.body)
+            res.send('Authorised user')
+        }
+        else{
+            res.send('UnAuthorised user')
+
+        }
+    })
+
+  })
+
   app.post("/api/login",async(req,res)=>{
 
     try{
@@ -32,21 +50,45 @@ mongoose.connect('mongodb+srv://userone:userone@cluster0.vcc0q.mongodb.net/BlogA
         console.log(req.body)
         var userEmail= req.body.email
         var userPass= req.body.password
-        let result= await userModel.find(
-            {$and:[
-                {email:userEmail},
-                {password:userPass}
-            ]
-        }
-            )
+        let result=  userModel.find({email:userEmail},(err,data)=>{
+            if(data.length>0){
+                const passwordValidator=bcrpt.compareSync(userPass,data[0].password)
+                console.log(passwordValidator)
+                if(passwordValidator)
+                {
+                    // token generation
+                    jwt.sign({email:data[0].email,id:data[0]._id},
+                        'ictacademy',
+                        {expiresIn:'1d'},
+                        (err,token)=>{
+                            if(err){
+                                res.json({status:'error in token generation'})
+                            }
+                            else{
+                                res.json({status:'login success',token:token})
+                            }
+                        }
 
-            if(result.length>0)
-            {
-                res.json({status:'success',data:result})
+                    )
+
+
+                    /////////
+
+
+                }
+                else{
+                    res.json({status:'invalid password'})
+
+                }
+
             }
             else{
-                res.json({status:'authentication failed.'})
+                res.json({status:'invalid email id'})
+
             }
+        })
+
+          
 
 
     }
@@ -63,10 +105,34 @@ app.post("/api/register",async(req,res)=>{
 
     try{
 
+        userModel.find({email:req.body.email},(err,data)=>{
+if(data.length==0){
 
-        let user=new userModel(req.body)
-        let result=await user.save()
-         res.json({status:'sucesss'})
+    let user=new userModel({ name: req.body.name, 
+        email: req.body.email,
+        password: bcrpt.hashSync(req.body.password,10) })
+
+    let result= user.save( (err,data)=>{
+        if(err){
+            res.json({status:'error happened'})
+
+        }
+        else{
+            res.json({status:'sucesss'})
+
+        }
+    } )
+
+}
+else{
+    res.json({status:'email id already exists'})
+
+
+}
+
+        })
+
+       
 
     }
     catch(error)
